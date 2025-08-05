2ca05c4f39e0f2b5f39966f4bb6dd9acb0dbaa07
Suhail Rehman <suhailrehman@gmail.com>
2025-08-05 17:10:44 +0000

    Added hugo install script

---
+++ install-hugo.sh
@@ -0,0 +1,4 @@
+# Stupid hugo version needs to be 0.101.0 for "wowchemy" to work correctly:
+
+wget https://github.com/gohugoio/hugo/releases/download/v0.101.0/hugo_extended_0.101.0_Linux-64bit.deb
+sudo dpkg -i hugo_extended_0.101.0_Linux-64bit.deb
